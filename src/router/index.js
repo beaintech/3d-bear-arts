40e297289b03f34303a6c79a03c525a3a3a68bcc
Bea <bea.mondayrocks@gmail.com>
2024-10-13 22:52:41 +0200

    fix: add mouse function

--- src/router/index.js
+++ src/router/index.js
@@ -8,12 +8,11 @@ import BlueBear from '../components/BlueBear.vue';
 import BluePinkBear from '../components/BluePinkBear.vue';
 import Transparent from '../components/HalfTransparentBear.vue';
 import HalfBear from '../components/HalfBlueBear.vue';
-import SkyBear from '../components/SkyBear.vue';
 import SliverBear from '../components/SliverBear.vue';
-
 const routes = [
     { path: '/3d-bear-arts', name: 'ThreeScene', component: ThreeScene },
     { path: '/3d-bear-arts/half', name: 'NewBear', component: HalfBear },
+    { path: '/3d-bear-arts/sliver', name: 'SliverBear', component: SliverBear },
     { path: '/3d-bear-arts/halfTransparent', name: 'Transparent', component: Transparent },
     { path: '/3d-bear-arts/bluePink', name: 'BluePinkBear', component: BluePinkBear },
     { path: '/3d-bear-arts/diamond', name: 'DiamondBear', component: DiamondBear },
@@ -21,8 +20,6 @@ const routes = [
     { path: '/3d-bear-arts/purple', name: 'PurpleBear', component: PurpleBear },
     { path: '/3d-bear-arts/blue', name: 'BlueBear', component: BlueBear },
     { path: '/3d-bear-arts/glass', name: 'GlassBear', component: GlassBear },
-    { path: '/3d-bear-arts/sky', name: 'SkyBear', component: SkyBear },
-    { path: '/3d-bear-arts/sliver', name: 'SliverBear', component: SliverBear },
 ];
 const router = createRouter({
     history: createWebHistory(),
